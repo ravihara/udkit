1ffb2711474788a599212a6a057fb816d6b86247
Ravishankar Haranath <ravikh@gmail.com>
2023-02-18 23:59:50 +0530

    Added kubctl and pyenv setup scripts in skel

---
+++ skel/kube-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir -p ~/.local/bin
+
+pushd ~/.local/bin >/dev/null
+
+## Removed existing kubectl and kubectl-convert
+rm -f kubectl kubectl-convert && sync
+
+## Download the kubectl and kubectl-convert
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert"
+
+## Validate the binaries with checksum
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+echo "$(cat kubectl-convert.sha256) kubectl-convert" | sha256sum --check
+
+## Setup executable bit
+chmod +x kubectl kubectl-convert
+rm -f *.sha256 && sync
+
+## Remove existing helm binary
+rm -f helm && sync
+
+## Install helm from installation script
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+USE_SUDO=false HELM_INSTALL_DIR=$HOME/.local/bin ./get_helm.sh && sync
+
+## Cleanup installation script
+rm -f get_helm.sh
+
+popd >/dev/null
---
+++ skel/pyenv-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LATEST_PYVER=3.11.2
+
+## Install core dependencies
+sudo apt update -y && apt upgrade -y
+sudo apt install -y build-essential g++ make automake autoconf libtool intltool colormake cmake \
+    curl rsync wget htop jq tree git bzip2 zip unzip net-tools vim universal-ctags gpg apt-transport-https
+
+## Install python dependencies
+sudo apt -y install zlib1g-dev build-essential libgdbm-dev libncurses5-dev libssl-dev libnss3-dev \
+    libffi-dev libreadline-dev wget libsqlite3-dev libbz2-dev uuid-dev liblzma-dev
+
+## Cleanup apt packages
+sudo apt autoremove --purge -y && sudo apt clean && sudo dpkg --configure -a
+
+## Install pyenv
+curl https://pyenv.run | bash
+
+## Install latest python3
+pyenv install $LATEST_PYVER && sync && pyenv global $LATEST_PYVER
+sync && cd $(pwd)
+
+## Install poetry
+curl -sSL https://install.python-poetry.org | python3 -
