f8b89319da03c4ec4f2f6108ab7f9d1415696684
Ravishankar Haranath <ravikh@gmail.com>
2024-12-18 15:07:42 +0530

    Updated env files, pyenv setup and direnvrc

--- env.zsh
+++ env.zsh
@@ -6,6 +6,46 @@ export UDKIT_BASE=$HOME/.udkit
 export TRML_HL='\033[1;35m'
 export TRML_NC='\033[0m'
 
+## Set default editor (Ex., for direnv)
+if [ -z "$EDITOR" ]; then
+  export EDITOR=vim
+fi
+
+## Export common environment variables needed by libraries and executables
+export PATH="$UDKIT_BASE/bin:$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+## Terminal settings for system debug related params
+ulimit -c unlimited
+
+## For GNUPG v2.0
+export GPG_TTY=$(tty)
+
+## Docker settings
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=0
+
+## Starship prompt configuration
+if [ -n "$(command -v starship 2>/dev/null)" ]; then
+  eval "$(starship init bash)"
+fi
+
+## Fuzzy finder configuration
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+## Rust/cargo configuration
+[ -d ~/.cargo ] && source "$HOME/.cargo/env"
+
+## Pyenv configuration
+if [ -n "$(command -v pyenv 2>/dev/null)" ]; then
+  export PYENV_ROOT=$(pyenv root)
+  export PATH="${PYENV_ROOT}/bin:$PATH"
+
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+fi
+
 ## Array to string conversion with given separator
 join_list_items_by() {
   local d=${1-} f=${2-}
@@ -35,8 +75,6 @@ normalize_build_env() {
 
   unset path_parts
 }
-
-export PATH="$UDKIT_BASE/bin:$PATH"
 
 ######### DO NOT EDIT ANYTHING BELOW THIS LINE #########
 ### direnv configuration - This should be at the end ###
--- skel/pyenv-setup.sh
+++ skel/pyenv-setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-LATEST_PYVER=${1:-3.11.9}
+LATEST_PYVER=${1:-3.11.11}
 
 ## Install system dependencies
-sudo apt update -y && sudo apt dist-upgrade -y
-sudo apt install -y fontconfig curl rsync wget htop jq tree git bzip2 zip unzip net-tools vim universal-ctags \
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y fontconfig curl rsync wget htop jq tree git bzip2 zip unzip net-tools vim universal-ctags \
   gpg xfonts-utils apt-transport-https locales tzdata libcap2-bin procps iproute2 nano xz-utils build-essential \
   make automake autoconf libtool intltool cmake pkg-config swig libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
   libsqlite3-dev libncursesw5-dev tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libgdbm-dev libnss3-dev \
   uuid-dev && sync
 
 ## Cleanup apt packages
-sudo apt autoremove --purge -y && sudo apt clean && sudo dpkg --configure -a
+sudo apt-get autoremove --purge -y && sudo apt-get clean && sudo dpkg --configure -a
 
 ## Install pyenv
-if [ -z "$(which pyenv 2>/dev/null)" ]; then
+if [ -z "$(command -v pyenv 2>/dev/null)" ]; then
   curl https://pyenv.run | bash
 
   export PATH="${HOME}/.pyenv/bin:$PATH"
@@ -38,7 +38,7 @@ pyenv global ${LATEST_PYVER} && sync
 python3 -m pip install -U --upgrade wheel setuptools pybind11 pip
 
 ## Install poetry
-if [ -z "$(which poetry 2>/dev/null)" ]; then
+if [ -z "$(command -v poetry 2>/dev/null)" ]; then
   curl -sSL https://install.python-poetry.org | python3 -
 else
   echo -e "python-poetry already installed, trying to update it..."
